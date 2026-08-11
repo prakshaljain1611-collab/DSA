c0bef271a832e76d622d3917f2ae68d0256e4cf3
prakshaljain1611-collab <prakshaljain1611@gmail.com>
2026-08-11 19:21:06 +0530

    Time: 8 ms (34.03%), Space: 28.9 MB (9.59%) - LeetHub

---
+++ 1670-design-front-middle-back-queue/1670-design-front-middle-back-queue.cpp
@@ -0,0 +1,57 @@
+class FrontMiddleBackQueue {
+public:
+    deque<int>d;
+    FrontMiddleBackQueue() {
+        
+    }
+    
+    void pushFront(int val) {
+        d.push_front(val);
+    }
+    
+    void pushMiddle(int val) {
+        d.insert(d.begin()+d.size()/2,val);
+    }
+    
+    void pushBack(int val) {
+        d.push_back(val);
+    }
+    
+    int popFront() {
+        if(d.empty()){
+            return -1;
+        }
+        int a=d.front();
+        d.pop_front();
+        return a;
+    }
+
+    int popMiddle() {
+        if(d.empty()){
+            return -1;
+        }
+        int a=d[(d.size()-1)/2];
+        d.erase(d.begin()+(d.size()-1)/2);
+        return a;
+    }
+    
+    int popBack() {
+        if(d.empty()){
+            return -1;
+        }
+        int a=d.back();
+        d.pop_back();
+        return a;
+    }
+};
+
+/**
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * FrontMiddleBackQueue* obj = new FrontMiddleBackQueue();
+ * obj->pushFront(val);
+ * obj->pushMiddle(val);
+ * obj->pushBack(val);
+ * int param_4 = obj->popFront();
+ * int param_5 = obj->popMiddle();
+ * int param_6 = obj->popBack();
+ */
